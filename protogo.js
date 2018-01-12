08cf44873c1653189b2330ac2ce4778810a06e92
jbears <designe@outlook.com>
2018-01-13 00:46:08 +0900

    base frame writed

--- protogo.js
+++ protogo.js
@@ -4,5 +4,23 @@
  *
  * Author : jbear; hello.designe@facebook.com
  * Contact : http://jbear.co
+ * Copyright @ jbear
  *
 **/
+
+window.protogo = (function() {
+    function protogo() {
+        var about = {
+            VERSION : '0.1',
+            AUTHOR : "jbear"
+        };
+    }
+    protogo.prototype = {
+        src: function(_src) {
+            console.log("Load data source ;)");
+            
+        }
+    };
+
+    return protogo;   
+}());

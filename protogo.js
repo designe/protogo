1c75064133ca478ab8748a2b949dd8bf46bcb99d
designe <hello.designe@facebook.com>
2018-04-28 20:46:29 +0900

    Protogo ES6 modulizing, and Refactoring

--- protogo.js
+++ protogo.js
@@ -2,22 +2,27 @@
  *
  * PROTOGO; Library for data prototyping
  *
- * Author : jbear; hello.designe@facebook.com
+ * It's ES6 Compatibility
+ *
+ * Author : jbear; JI-WOONG CHOI
  * Contact : http://jbear.co
  * Copyright @ jbear
  *
 **/
 
-window.Protogo = (function() {
+import { ChoiTrie } from "./choi-trie.js/choi-trie.js";
+
+export var Protogo = (function() {
     function protogo() {
         var about = {
             VERSION : '0.2',
             AUTHOR : "jbear"
         };
-        _proto = this;
+        this.P = this;
         this.MATCH_THRESHOLD = 2;
         this.raw = null;
         this.fields = {};
+        this.engine = null;//new ChoiTrie();
         // this.root = {};
     }
 
@@ -25,8 +30,8 @@ window.Protogo = (function() {
         init: function(_src) {
             var self = this;
 
-            _proto.raw = _src;
-            _proto.fields = {};
+            self.P.raw = _src;
+            self.P.fields = {};
 
             return this.add(_src);
         },
@@ -52,10 +57,10 @@ window.Protogo = (function() {
 
                     console.log(_key);
 
-                    if(typeof _proto.fields[_key] != 'undefined'){
+                    if(typeof self.P.fields[_key] != 'undefined'){
                         console.log(self.fields[_key]);
-                        _value_root = (typeof _proto.fields[_key].root == 'undefined') ? {} : _proto.fields[_key].root;
-                        _data_array = (typeof _proto.fields[_key].value_list == 'undefined') ? [] : _proto.fields[_key].value_list;
+                        _value_root = (typeof self.P.fields[_key].root == 'undefined') ? {} : self.P.fields[_key].root;
+                        _data_array = (typeof self.P.fields[_key].value_list == 'undefined') ? [] : self.P.fields[_key].value_list;
                     } else {
                         _value_root = {};
                         _data_array = [];
@@ -70,30 +75,32 @@ window.Protogo = (function() {
                     for(var i = 0;  i < word_count; i++) {
                         var _word = _src[i][_key];
                         _data_array.push(_word);
-                        self.addOnBasicTrie(_value_root, _word, _src[i]);
+                        self.addOnEngine(_value_root, _word, _src[i]);
+                        //self.addOnBasicTrie(_value_root, _word, _src[i]);
                     }
 
-                    _proto.fields[_key] = {
+                    self.P.fields[_key] = {
                         idx : _field_cnt,
                         value_list : _data_array,
                         root : _value_root,
                         search : function(_query){
-                            return self.searchOnBasicTrie(this.root, _query);
+                            return self.searchOnEngine(this.root, _query);
+                            //self.searchOnBasicTrie(this.root, _query);
                         }
                     };
 
-                    _proto[_key] = _proto.fields[_key];
+                    self.P[_key] = self.P.fields[_key];
                     _field_cnt++;
                 });
 
-                _proto.fields.names = _field_names;
-                _proto.fields.count = _field_cnt;
-                _proto.fields.get = (_idx) => {
-                    return _proto.fields.names[_idx];
+                self.P.fields.names = _field_names;
+                self.P.fields.count = _field_cnt;
+                self.P.fields.get = (_idx) => {
+                    return self.P.fields.names[_idx];
                 };
-                _proto.fields.order = _field_order;
+                self.P.fields.order = _field_order;
 
-                return _proto.fields;
+                return self.P.fields;
             }    /* end of source array */
             else {
                 return null;
@@ -111,158 +118,31 @@ window.Protogo = (function() {
                 console.log(_idx);
                 console.log(this.fields.names[_idx]);
                 var _field_result = this[this.fields.names[_idx]].search(_query);
+                if(_field_result){
+                    console.log("RESULT****");
+                    console.log(_field_result);
 
-                console.log(_field_result);
-
-                for(var j = 0; j < _field_result.length; j++)
-                    result.push(_field_result[j]);
+                    for(var j = 0; j < _field_result.length; j++)
+                        result.push(_field_result[j]);
+                }
             }
 
             return result;
         },
-        addOnMoebiousTrie: function(_root, _word, _what) {
-            var self = this;
-            var current = _root;
-
-            console.log(`log : ${_root}, ${_word}, ${_what}`);
-            var prefix = null;
-            var prefix_length = 0;
-            for(var i = _word.length; i >= 1; i--) {
-                var subword = _word.substr(0, i);
-                if(current[subword]) {
-                    prefix = subword;
-                    prefix_length = i;
-                    break;
-                }
-            }
-
-            if(prefix) {
-                //console.log(`1 log : ${prefix}`);
-                self.addOnMoebiousTrie(current[prefix], _word.substr(prefix_length, _word.length), _what);
-            }
-            else {
-                //console.log(`2 log : ${_word}`);
-                var rootKeys = null;//Object.keys(current);
-                if(current.__object$) {
-                    var current_temp = Object.assign({}, current);
-                    delete current_temp.__object$;
-                    rootKeys = Object.keys(current_temp);
-                } else {
-                    rootKeys = Object.keys(current);
-                }
-
-                var found_check = false;
-                if(rootKeys.length > 0) {
-                    rootKeys.some(function(_key) {
-                        if(_key[0] == _word[0]) {
-                            found_check = true;
-                            
-                            prefix = _key[0];
-                            prefix_length = 1;
-                            for(; prefix_length < _key.length; prefix_length++) {
-                                if(_key[prefix_length] != _word[prefix_length]){
-                                    prefix = _word.substr(0, prefix_length);
-                                    break;
-                                }
-                            }
-
-                            console.log(`${prefix}, ${_key}, ${_word}`);
-                            current[prefix] = {};
-                            current[prefix][_key.substr(prefix_length, _key.length - prefix_length)] = current[_key];
-                            delete current[_key];
-
-                            if(prefix.localeCompare(_word)){
-                                current[prefix][_word.substr(prefix_length, _word.length - prefix_length)] = {};
-                                current[prefix][_word.substr(prefix_length, _word.length - prefix_length)].__object$ = _what;
-                            }else
-                                current[prefix].__object$ = _what;
-                            
-                        }
-                    });
-                }
-
-                if(!found_check){
-                    current[_word] = {};
-                    current[_word].__object$ = _what;
-                }
-            }
+        addOnEngine: function(_root, _word, _what) {
+            if(this.engine){
+                this.engine.setRoot(_root);
+                return this.engine.add(_word, _what);
+            } else
+                return this.addOnBasicTrie(_root, _word, _what);
         },
-        searchOnMoebiousTrie: function(_root, _query) {
-            var _queryResult = [];
-
-            var _current = _root;
-            var _query_instance = _query;
-            var _query_instance_length = _query_instance.length;
-
-            var found_check = false;
+        searchOnEngine: function(_root, _word) {
+            if(this.engine) {
+                this.engine.setRoot(_root);
+                return this.engine.search(_word);
+            } else
+                return this.searchOnBasicTrie(_root, _word);
             
-            while(_query_instance_length != 0){
-                var prefix = _query_instance.substr(0, _query_instance_length);
-                if(_current[prefix]) {
-                    _current = _current[prefix];
-
-                    _query_instance = _query_instance.substr(prefix.length, _query_instance.length - prefix.length);
-                    _query_instance_length = _query_instance.length;
-                    
-                    if(_query_instance_length == 0){
-                        found_check = true;
-                        console.log(`${prefix}, ${_current.__object$}`);
-                        break;
-                    }
-                } else {
-                    _query_instance_length--;
-                }
-            }
-
-            if(!found_check) {
-                _current = _root;
-
-                var keysArray = Object.keys(_current);
-                keysArray.some(function(_key) {
-                    if(_query[0] == _key[0]) {
-                        found_check = true;
-
-                        var min_length = (_query.length > _key.length) ? _key.length : _query.length;
-                        for(var i = 1; i < min_length; i++) {
-                            if(_query[i] != _key[i])
-                                found_check = false;
-                        }
-
-                        console.log(`${min_length}, ${found_check}`);
-
-                        if(found_check) {
-                            // full search on prefix
-                            var queue_result = [];
-                            var front = 0;
-                            var end = 1;
-                            queue_result.push(_key);
-                            if(_current[_key].__object$)
-                                _queryResult.push(_current[_key].__object$);
-                            
-                            do {
-                                for(var k in _current[queue_result[front]]) {
-                                    console.log(k);
-                                    console.log(queue_result);
-                                    if(k.localeCompare("__object$")) {
-                                        queue_result.push(k);
-                                        if(_current[queue_result[front]][k].__object$)
-                                            _queryResult.push(_current[queue_result[front]][k].__object$);
-                                        
-                                        end++;
-                                    }
-                                }
-
-                                _current = _current[queue_result[front]];
-                                console.log(`${front} , ${end}, ${_current}`);
-
-                                front++;
-                            } while(front != end);
-
-                            console.log(_queryResult);
-                        }
-                    }
-                });
-            }
         },
         addOnBasicTrie: function(_root, _word, _what) {
             var _data = _word;
@@ -313,8 +193,10 @@ window.Protogo = (function() {
             // trie search applied
             var _current = _root;
 
-            if(_proto.MATCH_THRESHOLD)
-                _MATCH_THRESHOLD = _proto.MATCH_THRESHOLD;
+            var _MATCH_THRESHOLD = 2;
+            
+            if(this.MATCH_THRESHOLD)
+                _MATCH_THRESHOLD = this.MATCH_THRESHOLD;
 
             var _idx = 0;
             for(var i = 0 ; i < _query.length; i++) {
